1ce1245a991375dba05774afe3563ca2c5002338
andrea ritossa <andre.ritossa@gmail.com>
2022-09-05 00:12:51 +0200

    1.0

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleKeyDown(event) {
+    if(event.which === 13){
+      this.search();
+    }
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyDown={this.handleKeyDown}/>
+        <a onClick={this.search}>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/Components/SongPlayer/Player.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Player extends React.Component{
+
+    
+
+    render () {
+        return(
+            <div>
+                <h1>Song Player</h1>
+            </div>
+        )
+    }
+}
